467ec5f889a8282ae3d7769577b157a2f6969148
Ape07Park <ape072399@gmail.com>
2024-06-30 10:57:40 +0900

    commit test

--- src/main/java/CustomerJpaExam.java
+++ src/main/java/CustomerJpaExam.java
@@ -8,7 +8,7 @@ public class CustomerJpaExam {
     public static void main(String[] args) {
         // EntityManager를 가져오기 위한 팩토리 설정
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("customer-exam"); // persistence.xml의 <persistence-unit name="customer-exam">을 따라감
-        // EntityManager 생성: jpa를 이용해 db에 저장 시 필요
+        // EntityManager(쿼리를 만들어 보내고 데이터 가져옴) 생성: jpa를 이용해 db에 저장 시 필요
         EntityManager em = emf.createEntityManager();
 
         /*
@@ -20,16 +20,51 @@ public class CustomerJpaExam {
         EntityTransaction tx = em.getTransaction();
         // 트랜젝션 시작
         tx.begin();
+
+         /*
+        CRUD 작업 시작
+         */
+        try {
+
         /*
         CRUD 작업
          */
-        em.persist(Customer.sample()); // 매개변수로 실제 넣을 객체 넣기
+              /*
+                C 작업
+               */
+//            em.persist(Customer.sample()); // 매개변수로 실제 넣을 객체 넣기
 
-        // 트랜젝션 커밋 즉 CRUD 작업 적용
-        tx.commit();
+            /*
+                R 작업
+            */
+            // *find() 메서드는 EntityManager가 DB에서 가져온 데이터를 기본생성자를 이용해 인스턴스를 생성함
+            Customer foundCustomer = em.find(Customer.class, "ID0001"); // 매개변수로 가져올 객체, PK 지정
+            System.out.println("가져온 customer의 내용물" + foundCustomer.toString());
 
-        // 자원 반환
-        em.close();
+            /*
+                U 작업: 반드시 먼저 R 작업 필요
+             */
+
+            // set으로 변경
+//            foundCustomer.setName("Park");
+            System.out.println("수정한 customer의 내용물" + foundCustomer.toString());
+
+            /*
+                D 작업
+             */
+            em.remove(foundCustomer);
+            System.out.println("제거 customer의 내용물" + foundCustomer.toString());
+
+            // 트랜젝션 커밋 즉 CRUD 작업 적용. 트랜젝션 커밋 시 EntityManager의 관리 하에 있는 것은 업데이트된 것이 자동 반영됨
+            tx.commit();
+        } catch (Exception e) {
+
+            tx.rollback();
+        } finally {
+            // 자원 반환
+            em.close();
+
+        }
         emf.close();
     }
 }
--- src/main/java/entity/Customer.java
+++ src/main/java/entity/Customer.java
@@ -3,7 +3,12 @@ package entity;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.*;
 
+@ToString
+@NoArgsConstructor
+@Setter
+@Getter
 @Entity // jpa를 이용해 데이터가 들어갈 객체임을 표시
 @Table(name="customer_tb") // 어떤 테이블에 데이터 CRUD 할지
 public class Customer {
@@ -11,8 +16,6 @@ public class Customer {
     private String id;
     private String name;
     private long registerDate;
-
-    public Customer() {}
 
     public Customer(String id, String name) {
         this.id = id;
